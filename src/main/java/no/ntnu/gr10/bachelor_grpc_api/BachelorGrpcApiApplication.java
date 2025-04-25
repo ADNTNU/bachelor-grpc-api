29d893cb1e806092cd0e87c2bb6e0a40415a3ca3
Daniel Neset <danielneset@gmail.com>
2025-04-25 23:18:01 +0200

    Initial commit with spring boot initializr

---
+++ src/main/java/no/ntnu/gr10/bachelor_grpc_api/BachelorGrpcApiApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr10.bachelor_grpc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorGrpcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorGrpcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ntnu/gr10/bachelor_grpc_api/BachelorGrpcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.gr10.bachelor_grpc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorGrpcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
